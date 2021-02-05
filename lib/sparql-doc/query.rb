f79ce38d544a2c16ae9b4b3d656634dc788cabd9
Leigh Dodds <leigh@ldodds.com>
2021-02-05 18:17:38 +0000

    Removing warnings

--- lib/sparql-doc/query.rb
+++ lib/sparql-doc/query.rb
@@ -1,22 +1,22 @@
 module SparqlDoc
 
-  #Wrapper for SPARQL query  
+  #Wrapper for SPARQL query
   class Query
-    
+
     attr_reader :path, :query, :raw_query, :prefixes, :type, :package
 
     ANNOTATIONS = {
       :author => {
-        :multi => true 
+        :multi => true
       },
       :see => {
-        :multi => true 
-      },  
+        :multi => true
+      },
       :tag => {
-        :multi => true 
-      },  
+        :multi => true
+      },
       :title => {
-        :multi => false 
+        :multi => false
       },
       :endpoint => {
         :multi => true
@@ -25,11 +25,11 @@ module SparqlDoc
         :multi => true
       }
     }
-        
+
     ANNOTATIONS.each do |var, config|
       attr_reader(var)
     end
-    
+
     def initialize(path, query, package={})
       ANNOTATIONS.each do |var, config|
         if config[:multi]
@@ -40,23 +40,23 @@ module SparqlDoc
       end
       @path = path
       @query = query
-      @raw_query = query      
+      @raw_query = query
       @title = @path
       @description = ""
       @prefixes = {}
-        
+
       ["endpoint", "author", "tag"].each do |annotation|
         if package[annotation]
           instance_variable_set( "@#{annotation}", package[annotation])
         end
-      end  
+      end
       parseQuery()
     end
-    
+
     def output_filename
       return "#{path.gsub(".rq", "")}.html"
     end
-    
+
     def description(html=false)
       if html
         renderer = Redcarpet::Render::HTML.new({})
@@ -65,45 +65,45 @@ module SparqlDoc
       end
       @description
     end
-    
+
     def query_string
       CGI::escape( @query )
     end
-    
+
     private
-    
+
       def parseQuery
         query_lines = []
         header = true
-        description = false 
+        description = false
         description_lines = []
         @raw_query.split("\n").each do |line|
           if ( header && line.match(/^#/) )
             if ( matches = line.match(/^# *@([a-zA-Z]+) *(.+)$/i) )
               annotation = matches[1]
-              config = ANNOTATIONS[ annotation.intern ] 
+              config = ANNOTATIONS[ annotation.intern ]
               if config
                 if config[:multi]
                   val = instance_variable_get("@#{annotation}")
                   val << matches[2].strip
                 else
                   instance_variable_set("@#{annotation}", matches[2].strip)
-                end                
+                end
                 description = true
               else
                 $stderr.puts("Ignoring unknown annotation: @#{annotation}")
               end
-            else if (description == false) 
+            else if (description == false)
               description_lines << line[1..-1].strip
             end
           end
           else
             header = false
-            query_lines << line  
+            query_lines << line
           end
         end
         @description = description_lines.join("\n") unless description_lines.empty?
-        @query = query_lines.join("\n") unless query_lines.empty?    
+        @query = query_lines.join("\n") unless query_lines.empty?
         @query.strip!
         query_lines.each do |line|
           if (matches = line.match(/^ *PREFIX *([a-zA-Z_-]+) *: *<(.+)>$/i) )
@@ -112,7 +112,7 @@ module SparqlDoc
           if (matches = line.match(/^ *(SELECT|CONSTRUCT|DESCRIBE|ASK) */i) )
             @type = matches[1].upcase
           end
-        end              
+        end
       end
   end
 end
--- tests/tc_query.rb
+++ tests/tc_query.rb
@@ -3,8 +3,8 @@ require 'sparql-doc'
 require 'test/unit'
 
 class QueryTest < Test::Unit::TestCase
-  
-  def test_basic    
+
+  def test_basic
     query = SparqlDoc::Query.new("/path/to/query.rq", "DESCRIBE ?x")
     assert_equal("/path/to/query.rq", query.title)
     assert_equal("", query.description)
@@ -14,19 +14,19 @@ class QueryTest < Test::Unit::TestCase
     assert_equal([], query.author)
     assert_equal([], query.endpoint)
     assert_equal([], query.param)
-    assert_equal("DESCRIBE ?x", query.query)  
+    assert_equal("DESCRIBE ?x", query.query)
     assert_equal("DESCRIBE ?x", query.raw_query)
     assert_equal("DESCRIBE+%3Fx", query.query_string)
     assert_equal("DESCRIBE", query.type)
   end
 
-  def test_type    
+  def test_type
     query = SparqlDoc::Query.new("/path/to/query.rq", "describe ?x")
     assert_equal("DESCRIBE", query.type)
-  end    
-  
+  end
+
   def test_prefixes
-    sparql=<<-EOL     
+    sparql=<<-EOL
 #Description
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 prefix foo: <http://example.org/foo>
@@ -35,28 +35,28 @@ prefix   baz  : <http://example.org/baz#>
 DESCRIBE ?x
 EOL
     query = SparqlDoc::Query.new("/path/to/query.rq", sparql)
-    assert_equal("DESCRIBE", query.type)    
+    assert_equal("DESCRIBE", query.type)
     assert_equal( 4, query.prefixes.size )
     assert_equal( "http://xmlns.com/foaf/0.1/", query.prefixes["foaf"] )
     assert_equal( "http://example.org/foo", query.prefixes["foo"] )
     assert_equal( "http://example.org/bar", query.prefixes["bar"] )
     assert_equal( "http://example.org/baz#", query.prefixes["baz"] )
   end
-  
-  def test_description    
-    sparql=<<-EOL     
+
+  def test_description
+    sparql=<<-EOL
 #Description
 DESCRIBE ?x
 EOL
     query = SparqlDoc::Query.new("/path/to/query.rq", sparql)
     assert_equal("/path/to/query.rq", query.title)
     assert_equal("Description", query.description)
-    assert_equal("DESCRIBE ?x", query.query)  
+    assert_equal("DESCRIBE ?x", query.query)
     assert_equal("#Description\nDESCRIBE ?x\n", query.raw_query)
   end
-  
-  def test_description    
-    sparql=<<-EOL     
+
+  def test_description_multiple_lines
+    sparql=<<-EOL
 #Description
 #Over multiple
 #...lines
@@ -65,73 +65,73 @@ EOL
     query = SparqlDoc::Query.new("/path/to/query.rq", sparql)
     assert_equal("/path/to/query.rq", query.title)
     assert_equal("Description\nOver multiple\n...lines", query.description)
-    assert_equal("DESCRIBE ?x", query.query)  
+    assert_equal("DESCRIBE ?x", query.query)
     assert_equal("#Description\n#Over multiple\n#...lines\nDESCRIBE ?x\n", query.raw_query)
-  end  
-  
-  def test_title    
-    sparql=<<-EOL     
+  end
+
+  def test_title
+    sparql=<<-EOL
 #Description
-# @title My Query    
+# @title My Query
 DESCRIBE ?x
 EOL
     query = SparqlDoc::Query.new("/path/to/query.rq", sparql)
     assert_equal("My Query", query.title)
     assert_equal("Description", query.description)
-    assert_equal("DESCRIBE ?x", query.query)  
+    assert_equal("DESCRIBE ?x", query.query)
   end
-  
+
   def test_all
-    sparql=<<-EOL     
+    sparql=<<-EOL
 #Description
-# @title My Query   
+# @title My Query
 # @author leigh@ldodds.com
 # @tag demo
 # @tag test
-# @see http://github.com/ldodds/sparql-doc                     
+# @see http://github.com/ldodds/sparql-doc
 DESCRIBE ?x
 EOL
     query = SparqlDoc::Query.new("/path/to/query.rq", sparql)
     assert_equal("My Query", query.title)
     assert_equal("Description", query.description)
-    assert_equal("DESCRIBE ?x", query.query)  
+    assert_equal("DESCRIBE ?x", query.query)
     assert_equal(["leigh@ldodds.com"], query.author)
     assert_equal(["http://github.com/ldodds/sparql-doc"], query.see)
-    assert_equal(["demo", "test"], query.tag)            
-  end  
+    assert_equal(["demo", "test"], query.tag)
+  end
 
-  def test_markdown    
-    sparql=<<-EOL     
+  def test_markdown
+    sparql=<<-EOL
 #Description
 DESCRIBE ?x
 EOL
     query = SparqlDoc::Query.new("/path/to/query.rq", sparql)
-    assert_equal("<p>Description<p>", query.description(true))
-    assert_equal("DESCRIBE ?x", query.query)  
-  end  
-  
-  def test_markdown    
-    sparql=<<-EOL     
+    assert_equal("<p>Description</p>\n", query.description(true))
+    assert_equal("DESCRIBE ?x", query.query)
+  end
+
+  def test_markdown_more
+    sparql=<<-EOL
 #See [my website](http://example.org)
 DESCRIBE ?x
 EOL
     query = SparqlDoc::Query.new("/path/to/query.rq", sparql)
     assert_equal("<p>See <a href=\"http://example.org\">my website</a></p>\n", query.description(true))
-    assert_equal("DESCRIBE ?x", query.query)  
-  end    
+    assert_equal("DESCRIBE ?x", query.query)
+  end
 
   def test_provide_default_authors
     query = SparqlDoc::Query.new("/path/to/query.rq", "DESCRIBE ?x", {"author" => ["Leigh Dodds"]})
-    assert_equal(["Leigh Dodds"], query.author)          
-  end    
+    assert_equal(["Leigh Dodds"], query.author)
+  end
 
   def test_provide_default_endpoint
     query = SparqlDoc::Query.new("/path/to/query.rq", "DESCRIBE ?x", {"endpoint" => ["http://example.org"]})
-    assert_equal(["http://example.org"], query.endpoint)          
-  end    
+    assert_equal(["http://example.org"], query.endpoint)
+  end
 
   def test_provide_default_tag
     query = SparqlDoc::Query.new("/path/to/query.rq", "DESCRIBE ?x", {"tag" => ["foo"]})
-    assert_equal(["foo"], query.tag)          
-  end    
+    assert_equal(["foo"], query.tag)
+  end
 end
